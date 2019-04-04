1eef3cedfcbcef4402f9971d5a4e6af1f1e08ad0
thomasryantama <thomasryantama@gmail.com>
2019-04-04 16:23:09 +0700

    belajar

--- belajar2.py
+++ belajar2.py
@@ -1,1 +1,8 @@
-print("2")
+print("Program 1 ")
+
+satuan = int(input("masukan jumlah : "))
+l = int(input("harga : "))
+b = satuan*l
+print ("total bayar : ", b)
+
+# dalam program ini dan mungkin bebrapa yang lainnya akan amat sederhana. hal ini dilakukan demi efisiensi waktu.
