6b41695556a29f76193bb15fc7fe4811654bd001
thomasryantama <thomasryantama@gmail.com>
2019-04-04 18:26:40 +0700

    belajar

--- BANYAK.py
+++
@@ -1,9 +0,0 @@
-a=int (input("banayk barang ; "))
-b=int (input("harga barang : "))
-c= a*b
-if c > 100 :
-    d = c*0.50
-else :
-    e=0
-f=int (c-d)
-print ("bayar : " , f )
--- rangkap.py
+++ rangkap.py
@@ -1,9 +1,22 @@
-a=int (input("banayk barang ; "))
-b=int (input("harga barang : "))
-c= a*b
-if c > 100 :
-    d = c*0.50
+#cabang if else
+
+#a=int (input("banayk barang ; "))
+#b=int (input("harga barang : "))
+#c= a*b
+#if c > 100 :
+#    d = c*0.50
+#else :
+#    e=0
+#f=int (c-d)
+#print ("bayar : " , f )
+
+# if                                                       
+
+nilai = int(input("nilai : "))
+if nilai > 70 :
+  maka = " lulus"
+
 else :
-    e=0
-f=int (c-d)
-print ("bayar : " , f )
+    #nilai > 60:
+    maka = "tolol"
+print("keterangan = ",  maka, ", nilai : ", nilai)
