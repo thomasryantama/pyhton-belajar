b2aea2a6795cfc63730cbb5776b914411a62488d
thomasryantama <thomasryantama@gmail.com>
2019-05-22 14:28:05 +0700

    Thom

--- belajar2.py
+++ belajar2.py
@@ -6,3 +6,12 @@ b = satuan*l
 print ("total bayar : ", b)
 
 # dalam program ini dan mungkin bebrapa yang lainnya akan amat sederhana. hal ini dilakukan demi efisiensi waktu.
+
+#tambahan untuk mengulang
+
+print ("hai")
+
+a = int(input("masuk: "))
+b= int(input("sama :"))
+c= float (a) * float(b)
+print ("ggg : ,", c )
